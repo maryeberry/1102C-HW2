a08a699c7ceffe6d23c5629dfbf6877c7434b286
Mary Banzon <maryelizabeth.banzon@gmail.com>
2022-09-11 23:05:56 -0400

    Editing js file

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var age;
 firstName = "Mary";
 lastName = "Banzon";
 age = 23;
-alert("My name is " + firstName " " + lastName ".");
+alert("My name is " + firstName + " " + lastName + ".");
 alert("I am " + age + " years old.");
 console.log("I am having a good time. :D");
